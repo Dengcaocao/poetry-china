71b39d1ac4b33f1342f7bdcb2a7bfac3db5f8970
dw <1536698087@qq.com>
2023-07-10 13:21:54 +0800

    poetry-china | init

---
+++ src/components/layout/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Header></Header>
+  <Main></Main>
+</template>
+<script setup lang="ts">
+import Header from './header.vue'
+import Main from './main.vue'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="main">
+    <h1>home</h1>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.main {
+  h1 {
+    color: red;
+  }
+}
+</style>
