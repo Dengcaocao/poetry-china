acf7b9b97e94509c13e51bf2602507409f677831
dw <1536698087@qq.com>
2023-07-13 15:42:10 +0800

    header填充功能

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -4,9 +4,22 @@
       <img src="https://avatars.githubusercontent.com/u/30764933?s=200&v=4" alt="诗词中国🇨🇳" />
       <h3>Poetry China</h3>
     </a>
-    <div class="action"></div>
+    <div class="action">
+      <div class="item chapter">花间集 第一章</div>
+      <div class="item dictation">
+        <Switch v-model:value="store.isDictation" />
+      </div>
+      <div class="item annotate">开关注解</div>
+    </div>
   </header>
 </template>
+
+<script setup lang="ts">
+import { useConfigStore } from '@/stores/config'
+import Switch from '@/components/swtich/index.vue'
+
+const store = useConfigStore()
+</script>
 
 <style scoped lang="scss">
 @import '../../assets/main.scss';
@@ -32,13 +45,23 @@ header {
     }
   }
   .action {
-    width: 200px;
+    display: flex;
+    align-items: center;
+    width: 600px;
     height: 100%;
+    padding: 20px;
+    box-sizing: border-box;
     border-radius: 15px;
     background-color: #fff;
     box-shadow: 0 6px 16px -8px rgba(0, 0, 0, 0.08),
               0 9px 28px 0 rgba(0, 0, 0, 0.05),
               0 12px 48px 16px rgba(0, 0, 0, 0.03);
+    .item {
+      padding: 0 8px;
+    }
+    .chapter {
+      font-size: 18px;
+    }
   }
 }
 </style>
