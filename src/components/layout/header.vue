6ff700875a0cad63e9ae5befd774811abe3ce761
dw <1536698087@qq.com>
2023-07-17 10:43:32 +0800

    修改默写功能按钮的布局与样式

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -7,18 +7,45 @@
     <div class="action">
       <div class="item chapter">花间集 第一章</div>
       <div class="item dictation">
-        <Switch v-model:value="store.isDictation" />
+        <i
+          class="iconfont icon-zitiyulan"
+          :class="{active: store.isDictation}"
+          @click="status = !status">
+        </i>
+        <Transition name="menu-move">
+          <div class="menu" v-if="status">
+            <p class="tips">是否开启默写</p>
+            <div class="switch-box">
+              <Switch v-model:value="store.isDictation" />
+              <p>默写已{{store.isDictation ? '开启' : '关闭'}}</p>
+            </div>
+            <template v-if="store.isDictation">
+              <p class="tips">默写模式</p>
+              <div class="selector">
+                <select>
+                  <option :value="0">全部隐藏</option>
+                  <option :value="1">显示标点符号</option>
+                </select>
+              </div>
+            </template>
+          </div>
+        </Transition>
       </div>
-      <div class="item annotate">开关注解</div>
+      <div class="item annotate">
+        <i class="iconfont icon-zhongwenxianshi" :class="{active: store.isDictation}"></i>
+      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useConfigStore } from '@/stores/config'
 import Switch from '@/components/swtich/index.vue'
 
 const store = useConfigStore()
+
+const status = ref(false)
 </script>
 
 <style scoped lang="scss">
@@ -47,21 +74,98 @@ header {
   .action {
     display: flex;
     align-items: center;
-    width: 600px;
     height: 100%;
-    padding: 20px;
+    padding: $gap-large;
+    font-size: 18px;
     box-sizing: border-box;
-    border-radius: 15px;
+    border-radius: $radius-large;
     background-color: #fff;
-    box-shadow: 0 6px 16px -8px rgba(0, 0, 0, 0.08),
-              0 9px 28px 0 rgba(0, 0, 0, 0.05),
-              0 12px 48px 16px rgba(0, 0, 0, 0.03);
-    .item {
-      padding: 0 8px;
-    }
+    box-shadow: $shadow-small;
     .chapter {
-      font-size: 18px;
+      padding: 6px 12px;
+      margin-right: $gap-middle;
     }
+    .item {
+      border-radius: $radius-middle;
+      &:hover.chapter,
+      &:hover .iconfont {
+        color: #fff !important;
+        cursor: pointer;
+        background-color: $theme-color-bg-deep;
+      }
+      .iconfont {
+        padding: 2px 4px;
+        margin-left: 6px;
+        color: $text-color;
+        border-radius: $radius-middle;
+        font-size: 22px;
+        &.active {
+          color: $theme-color;
+        }
+      }
+    }
+    .dictation {
+      position: relative;
+      .menu {
+        position: absolute;
+        top: 33px;
+        left: 50%;
+        z-index: 88;
+        transform: translateX(-50%);
+        width: 180px;
+        padding: $gap-large;
+        border-radius: $radius-middle;
+        box-sizing: border-box;
+        background-color: #fff;
+        box-shadow: $shadow-large;
+        .tips {
+          font-size: 14px;
+          margin-bottom: $gap-middle;
+        }
+        .switch-box {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          font-size: 12px;
+          margin-bottom: $gap-large;
+        }
+        .selector {
+          select {
+            outline: none;
+            width: 80%;
+            height: 30px;
+            border: none;
+            box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 2.16);
+          }
+        }
+      }
+    }
+  }
+}
+.menu-move-enter-active {
+  animation: moveEnter 0.2s;
+}
+.menu-move-leave-active {
+  animation: moveLeave 0.2s;
+}
+@keyframes moveEnter {
+  0% {
+    top: 66px;
+    opacity: 0;
+  }
+  100% {
+    top: 33px;
+    opacity: 1;
+  }
+}
+@keyframes moveLeave {
+  0% {
+    top: 33px;
+    opacity: 1;
+  }
+  100% {
+    top: 66px;
+    opacity: 0;
   }
 }
 </style>
